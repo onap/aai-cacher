8800149c0e49b668a02524def86dda52c5635f48
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-01-10 14:23:20 +0530

    Remove this useless assignment to local variable
    Sonar fix - MongoConfig - Remove this useless assignment to local variable
    
    Issue-ID: AAI-2067
    Change-Id: I6884f0d81cbabd0505989871a1c3bf8ca62fabb0
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- src/main/java/org/onap/aai/cacher/common/MongoConfig.java
+++ src/main/java/org/onap/aai/cacher/common/MongoConfig.java
@@ -3,6 +3,8 @@
  * org.onap.aai
  * ================================================================================
  * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -88,7 +90,6 @@ public class MongoConfig {
 
         MongodStarter starter = MongodStarter.getDefaultInstance();
 
-        String bindIp = MONGO_DB_HOST;
         int port = MONGO_DB_PORT;
         IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
                 .net(new Net(port, Network.localhostIsIPv6())).configServer(false).build();
