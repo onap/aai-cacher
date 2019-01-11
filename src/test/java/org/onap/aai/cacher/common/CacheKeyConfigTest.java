aa1d08e65369a30f20a67fec0e13e32cb7938b15
bogumil_zebek <bogumil.zebek@nokia.com>
2019-01-11 13:27:12 +0100

    Fix sonar violations
    Change-Id: Ie724d74ca833a1ee16ff37203916a2231dba41ed
    Issue-ID: AAI-2070
    Signed-off-by: Zebek Bogumil <bogumil.zebek@nokia.com>

--- src/test/java/org/onap/aai/cacher/common/CacheKeyConfigTest.java
+++ src/test/java/org/onap/aai/cacher/common/CacheKeyConfigTest.java
@@ -1,16 +1,39 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright © 2017-2018 Nokia Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
 package org.onap.aai.cacher.common;
 
 
 import com.google.common.collect.Lists;
-
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.onap.aai.cacher.model.CacheKey;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Bogumil Zebek
@@ -28,8 +51,8 @@ public class CacheKeyConfigTest {
         List<CacheKey> cacheKeys = cacheKeyConfig.populateCacheKeyList();
 
         // then
-        Assertions.assertThat(cacheKeys.size()).isEqualTo(4);
-        Assertions.assertThat(
+        assertThat(cacheKeys.size()).isEqualTo(4);
+        assertThat(
                 cacheKeys.stream().allMatch(
                         it -> Lists.newArrayList("cloud-region", "complex", "pserver", "generic-vnf").contains(it.cacheKey)
                 )
@@ -39,7 +62,11 @@ public class CacheKeyConfigTest {
 
     private String loadFile(String pathToFile) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(pathToFile).getFile());
+        URL resource = classLoader.getResource(pathToFile);
+        if (resource == null) {
+            throw new InvalidPathException("Missing file.", String.format("File '%s' not found.", pathToFile));
+        }
+        File file = new File(resource.getFile());
         return new String(Files.readAllBytes(file.toPath()));
     }
 }
